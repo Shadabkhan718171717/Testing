99bcf50cc3c9d0acee649f4d42e4741675a43fad
Shadabkhan718171717 <144777196+Shadabkhan718171717@users.noreply.github.com>
2024-05-29 14:54:32 +0530

    Shadab.js

--- Shadab.js
+++ Shadab.js
@@ -8,12 +8,14 @@ const app = express();
 // Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual Telegram bot token
 const bot = new TelegramBot('6911535039:AAHXY9rO7I9UB9nZPQleAOZhTr5VlcLYs04', { polling: true });
 
+// Handle /start command
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     const introduction = `Welcome to the Bot!\n\nTo use this bot, simply type /code followed by your query.\nFor example: /code your_query_here\n\nThis bot was created by Shadab Khan.`;
     bot.sendMessage(chatId, introduction);
 });
 
+// Handle /code command
 bot.onText(/\/code (.+)/, async (msg, match) => {
     const chatId = msg.chat.id;
     const query = match[1];
@@ -22,16 +24,33 @@ bot.onText(/\/code (.+)/, async (msg, match) => {
     const waitMessage = await bot.sendMessage(chatId, 'Please wait while we process your request...');
 
     try {
-        const response = await axios.post('https://estatic-node-api.onrender.com/tool-sphere/api/code-gen', {
-            query: query
+        const response = await axios.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent', {
+            contents: [{
+                role: "user",
+                parts: [{
+                    text: query
+                }]
+            }]
+        }, {
+            headers: {
+                'x-goog-api-key': 'AIzaSyBBbRcsQIJRrqNSDONOm5IwJQnbElVCQvw'
+            }
         });
-        const responseBody = response.data.msg;
-        
-        // Update 'wait' message with the response
-        await bot.editMessageText(responseBody, { chat_id: chatId, message_id: waitMessage.message_id });
+
+        // Extracting the response text from the Gemini Pro API response
+        const generatedText = response.data.candidates[0].content.parts[0].text;
+
+        // Sending the generated text as a message to the chat
+        bot.sendMessage(chatId, generatedText);
+
+        // Delete 'wait' message
+        await bot.deleteMessage(chatId, waitMessage.message_id);
     } catch (error) {
         console.error('Error:', error);
-        await bot.editMessageText('An error occurred while processing your request.', { chat_id: chatId, message_id: waitMessage.message_id });
+        bot.sendMessage(chatId, 'An error occurred while processing your request.');
+
+        // Delete 'wait' message
+        await bot.deleteMessage(chatId, waitMessage.message_id);
     }
 });
 
